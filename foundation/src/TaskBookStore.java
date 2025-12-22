db90a51debd5d32b674faa692d04008ec8771774
imammunf <imammunif17@gmail.com>
2025-12-22 15:08:35 +0700

    day 01: task (pseudo code)

---
+++ foundation/src/TaskBookStore.java
@@ -0,0 +1,32 @@
+public class TaskBookStore {
+
+    public static void main(String[] args) {
+        /* EXPECTED RESULT
+            === Book Store ===
+            1. View Book
+            2. Buy Book
+            3. Exit
+            Pilih [1-3] : 1
+            === View Book ===
+            1. Java @20.000
+            2. Spring @50.000
+
+            === Book Store ===
+            1. View Book
+            2. Buy Book
+            3. Exit
+            Pilih [1-3] : 2
+            Pilih Buku [1-2] : 2
+            Masukkan jumlah beli : 2
+            === Detail ===
+            Anda membeli buku Spring @50.000 sebanyak 2 dengan total Rp100.000
+
+            === Book Store ===
+            1. View Book
+            2. Buy Book
+            3. Exit
+            Pilih [1-3] : 3
+            === Thanks ===
+         */
+    }
+}
