c7fe384410000eab2a50f7505ae248c4a64792fc
imammunf <imammunif17@gmail.com>
2025-12-22 17:34:42 +0700

    day 01: task (implement feedback)

--- foundation/src/TaskBookStore.java
+++ foundation/src/TaskBookStore.java
@@ -3,37 +3,7 @@ import java.util.Scanner;
 public class TaskBookStore {
 
     public static void main(String[] args) {
-        /* EXPECTED RESULT
-            === Book Store ===
-            1. View Book
-            2. Buy Book
-            3. Exit
-            Pilih [1-3] : 1
-            === View Book ===
-            1. Java @20.000
-            2. Spring @50.000
-
-            === Book Store ===
-            1. View Book
-            2. Buy Book
-            3. Exit
-            Pilih [1-3] : 2
-            Pilih Buku [1-2] : 2
-            Masukkan jumlah beli : 2
-            === Detail ===
-            Anda membeli buku Spring @50.000 sebanyak 2 dengan total Rp100.000
-
-            === Book Store ===
-            1. View Book
-            2. Buy Book
-            3. Exit
-            Pilih [1-3] : 3
-            === Thanks ===
-         */
-
-        TaskBookStore store = new TaskBookStore();
-        store.mainMenu();
-
+        new TaskBookStore().mainMenu();
     }
 
     public void mainMenu() {
@@ -41,21 +11,20 @@ public class TaskBookStore {
         Scanner scanner = new Scanner(System.in);
         String[] arrBooks = {"Laskar Pelangi", "Kelinci vs Kura-kura", "Marmut Merah Jambu", "Mars vs Venus", "Jurus cepat masuk CPNS"};
         double[] arrPrices = {20000, 50000, 35000, 10000, 43000};
-        int optionInput;
 
         System.out.println("\n=== Book Store ===");
         System.out.println(" 1. View Book");
         System.out.println(" 2. Buy Book");
         System.out.println(" 3. Exit");
         System.out.print("Pilih [1-3] : ");
-        optionInput = scanner.nextInt();
+        int optionInput = scanner.nextInt();
 
         if (optionInput == 1) {
             viewBook(arrBooks, arrPrices);
         } else if (optionInput == 2) {
             buyBook(arrBooks, arrPrices);
         } else if (optionInput == 3) {
-            exitStore();
+            System.out.println("Thanks");
         } else {
             System.out.println("Invalid input");
             mainMenu();
@@ -66,27 +35,20 @@ public class TaskBookStore {
 
         System.out.println("=== View Book ===\n");
         for (int i = 0; i < arrBooks.length; i++) {
-            int number = i + 1;
-            System.out.println(number + ". " + arrBooks[i] + " (@" + arrPrices[i] + ")");
+            System.out.println((i+1) + ". " + arrBooks[i] + " (@" + arrPrices[i] + ")");
         }
         mainMenu();
     }
 
     public void buyBook(String[] arrBooks, double[] arrPrices) {
         Scanner scanner = new Scanner(System.in);
-        int selected;
-        int qty;
-        double total;
         System.out.print("Pilih Buku : ");
-        selected = scanner.nextInt();
+        int selected = scanner.nextInt();
         System.out.print("Masukkan jumlah beli : ");
-        qty = scanner.nextInt();
-        total = arrPrices[selected - 1] * qty;
+        int qty = scanner.nextInt();
+        double total = arrPrices[selected - 1] * qty;
         System.out.printf("Anda membeli buku " + arrBooks[selected - 1] + " sebanyak " + qty + " dengan total " + total);
         mainMenu();
     }
 
-    public void exitStore() {
-        System.out.println("Thanks");
-    }
 }
