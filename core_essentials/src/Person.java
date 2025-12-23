0b6f60279827719722d31b7a3a619a6db5667bc4
imammunf <imammunif17@gmail.com>
2025-12-23 10:12:16 +0700

    day 02: variable/field calling coverage

--- core_essentials/src/Person.java
+++ core_essentials/src/Person.java
@@ -1,5 +1,7 @@
 public class Person extends BasePerson {
 
+    private int age;
+
     public Person() {
         System.out.println("constructor: exec 2nd");
     }
@@ -10,6 +12,10 @@ public class Person extends BasePerson {
     }
 
     public void callTest() {
+        int age = 10;
+        int result1 = age;
+        int result2 = this.age;
+
         test(); // call the closest, else: look in parent
         this.test(); // call the method in the same class
         super.test(); // call the first parent method
