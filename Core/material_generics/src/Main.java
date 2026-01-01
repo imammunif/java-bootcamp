660eb5f8b286784fae6af96878978e9bf079f766
imammunif <imammunif17@gmail.com>
2026-01-01 11:45:55 +0700

    core: learn generics (why use generic)

--- Core/material_generics/src/Main.java
+++ Core/material_generics/src/Main.java
@@ -1,17 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Object> list = new ArrayList<>();
-        // the same
-        List list1 = new ArrayList<>();
+        Response response = new Response();
 
-        list.add("data");
-        list.add(true);
-        list1.add("data");
-        list1.add(true);
-        String data1 = (String) list.get(1); // not error, by Casting the Parent
-        String data2 = (String) list1.get(1); // not error, by Casting the Parent
+        response.setData("data");
+        response.setMessage("Success");
     }
 }
---
+++ Core/material_generics/src/Response.java
@@ -0,0 +1,17 @@
+public class Response {
+    private String data; // DATA CAN BE VARIOUS TYPE
+    private String message;
+
+    public String getData() {
+        return data;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
