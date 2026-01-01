3540ebf5ee393497b89618041a2352cfb82d73d3
imammunif <imammunif17@gmail.com>
2026-01-01 17:40:34 +0700

    core: learn functional interface (init project)

---
+++ Core/material_functionalInterface/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+//        Response<String> response1 = new Response<>(); // this will error
+//        response1.setData("data");
+//        response1.setMessage("Success");
+
+//        Response<Boolean> response2 = new Response<>(); // this will error
+//        response2.setData(true);
+//        response2.setMessage("Success");
+
+        Response<Person> response3 = new Response<>();
+        response3.setData(new Person());
+        response3.setMessage("Success");
+
+    }
+}
---
+++ Core/material_functionalInterface/src/Response.java
@@ -0,0 +1,22 @@
+public class Response<T extends Person> {
+    private T data; // DATA CAN BE VARIOUS TYPE
+    private String message;
+
+    public T getData() {
+        return data;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // defining its own parameter, with return
+    public <M> M  test(M param) {
+        return param;
+    }
+}
