17c71e2b25d7a7ca5777a21b11df2d8d28ff212b
imammunif <imammunif17@gmail.com>
2026-01-03 09:15:21 +0700

    day 07: implement reviewer notes (sequence capital only)

--- Task/MiniStore/src/com/dansmultipro/ministore/util/RandomSequence.java
+++ Task/MiniStore/src/com/dansmultipro/ministore/util/RandomSequence.java
@@ -3,9 +3,7 @@ package com.dansmultipro.ministore.util;
 // source Geeksforgeeks
 public class RandomSequence {
     public static String getAlphaNumericString(int n) {
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                + "0123456789"
-                + "abcdefghijklmnopqrstuvxyz";
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
         StringBuilder sb = new StringBuilder(n);
         for (int i = 0; i < n; i++) {
             int index
