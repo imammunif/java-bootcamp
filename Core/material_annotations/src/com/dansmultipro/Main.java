885790acd321cbc442ef2c1a683070aa585324a7
imammunf <imammunif17@gmail.com>
2026-01-05 10:23:27 +0700

    day 08: implementing reflection to invoke object into desired method

--- Core/material_annotations/src/com/dansmultipro/Main.java
+++ Core/material_annotations/src/com/dansmultipro/Main.java
@@ -24,26 +24,36 @@ public class Main {
 
     }
 
+    /*
+    * 1. Class
+    * 2. Method
+    * 3. Object
+    */
+
     public static void main(String[] args) throws Exception {
         // Reflection
         Class<?> clazz = Class.forName("com.dansmultipro.Main");
+        // Get the 3rd constructor with 2 parameters
+        Constructor<?> constructor = clazz.getConstructor(String.class, String.class);
+        Object obj = constructor.newInstance("param1", "param2");
+        Method m  = clazz.getMethod("test");
+        m.invoke(obj);
 
-        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
-        for (Constructor<?> constructor : constructors) {
-            System.out.println("\n" + constructor.getName());
-        }
-
-        for (Method m : clazz.getDeclaredMethods()) {
-            System.out.println("\n" + m.getName());
-        }
-
-        for (Field f : clazz.getDeclaredFields()) {
-            System.out.println("\n"+ f.getName());
-        }
+        //    for (Constructor<?> constructor : constructors) {
+        //        System.out.println("\n" + constructor.getName());
+        //    }
+        //
+        //    for (Method m : clazz.getDeclaredMethods()) {
+        //        System.out.println("\n" + m.getName());
+        //    }
+        //
+        //    for (Field f : clazz.getDeclaredFields()) {
+        //        System.out.println("\n"+ f.getName());
+        //    }
     }
 
     @onCreate(priority = 1)
     public void test() {
-
+        System.out.println("test...1....2....3");
     }
 }
