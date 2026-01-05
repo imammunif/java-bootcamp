585b5399dc3d66b1e08c4cc66dff780d9c517ef1
imammunf <imammunif17@gmail.com>
2026-01-05 09:46:06 +0700

    day 08: learn annotation & reflection

---
+++ Core/material_annotations/src/com/dansmultipro/Main.java
@@ -0,0 +1,15 @@
+package com.dansmultipro;
+
+import com.dansmultipro.annotation.onCreate;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+    }
+
+    @onCreate(priority = 1)
+    public void test() {
+
+    }
+}
---
+++ Core/material_annotations/src/com/dansmultipro/annotation/onCreate.java
@@ -0,0 +1,13 @@
+package com.dansmultipro.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface onCreate {
+    String value() default "";
+    int priority();
+}
