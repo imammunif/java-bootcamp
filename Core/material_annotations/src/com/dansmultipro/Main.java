605e681b70e73646b297f3b2f6287b1284654229
imammunf <imammunif17@gmail.com>
2026-01-05 10:07:39 +0700

    day 08: implementing reflection to check constructors

--- Core/material_annotations/src/com/dansmultipro/Main.java
+++ Core/material_annotations/src/com/dansmultipro/Main.java
@@ -2,10 +2,34 @@ package com.dansmultipro;
 
 import com.dansmultipro.annotation.onCreate;
 
-public class Main {
-    
-    public static void main(String[] args) {
+import java.lang.reflect.Constructor;
 
+public class Main {
+
+    private String name;
+    private int age;
+
+    // 1st Constructor (Empty constructor for testing other overload constructor)
+    public Main() {
+    }
+
+    // 2nd Constructor
+    public Main(String param1) {
+    }
+
+    // 3rd Constructor
+    public Main(String param1, String param2) {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Reflection
+        Class<?> clazz = Class.forName("com.dansmultipro.Main");
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println("\n" + constructor.getName());
+        }
     }
 
     @onCreate(priority = 1)
