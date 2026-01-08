0819f2d1da2cc7bb7eba93765f4838929bcc3445
imammunf <imammunif17@gmail.com>
2026-01-08 10:27:02 +0700

    day 11: inner and left join

--- SQL/day 11 DQL/mentor-script-DQL.sql
+++ SQL/day 11 DQL/mentor-script-DQL.sql
@@ -4,11 +4,16 @@ SELECT * FROM food_order;
 
 -- NOTES:
 -- INNER JOIN: Shows intersect data (beririsan)
-SELECT
-	*
-FROM
-	food_order fo
-INNER JOIN
-	driver d
-ON
-	fo.driver_id = d.id;
+SELECT * FROM food_order fo
+INNER JOIN driver d
+ON fo.driver_id = d.id;
+
+-- NOTES:
+-- LEFT JOIN: Shows data based main table FROM (will show even if it's empty)
+SELECT * FROM driver dr
+LEFT JOIN food_order fo
+ON fo.driver_id = dr.id;
+-- depends on the selected table FROM
+SELECT * FROM food_order fo
+LEFT JOIN driver d
+ON fo.driver_id = d.id;
