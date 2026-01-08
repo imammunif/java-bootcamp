2d04eca468d8b168dc4fd6e9d6fed163b85272a0
imammunf <imammunif17@gmail.com>
2026-01-08 17:16:32 +0700

    day 11: aggregate & grouping

--- SQL/day 11 DQL/mentor-script-DQL.sql
+++ SQL/day 11 DQL/mentor-script-DQL.sql
@@ -17,3 +17,32 @@ ON fo.driver_id = dr.id;
 SELECT * FROM food_order fo
 LEFT JOIN driver d
 ON fo.driver_id = d.id;
+
+-- NOTES:
+-- AGGREGATION: get summary from table
+--- COUNT: count data exist
+--- recomended to count by id or all fiels *
+SELECT COUNT(id) FROM driver;
+--- SUM: calculate total
+SELECT SUM(id) FROM driver;
+--- AVG: calculate average
+SELECT AVG(id) FROM driver;
+--- MAX: get maximum value
+SELECT MAX(id) FROM driver;
+--- MIN: get maximum value
+SELECT MIN(id) FROM driver;
+
+
+-- NOTES:
+-- GROUPING: combine same value
+--- COUNT: count data exist
+--- recomended to count by id or all fiels *
+SELECT COUNT(id) FROM driver;
+--- SUM: calculate total
+SELECT SUM(id) FROM driver;
+--- AVG: calculate average
+SELECT AVG(id) FROM driver;
+--- MAX: get maximum value
+SELECT MAX(id) FROM driver;
+--- MIN: get maximum value
+SELECT MIN(id) FROM driver;
