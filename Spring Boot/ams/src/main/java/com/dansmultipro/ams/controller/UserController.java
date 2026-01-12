1ea6a53b3e086982ef179f01b8b0e08890170652
imammunf <imammunif17@gmail.com>
2026-01-12 10:16:26 +0700

    day 13: get controller (shorter form)

--- Spring Boot/ams/src/main/java/com/dansmultipro/ams/controller/UserController.java
+++ Spring Boot/ams/src/main/java/com/dansmultipro/ams/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.dansmultipro.ams.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("user")
 public class UserController {
 
-    @GetMapping("/user/welcome")
+    @GetMapping("welcome")
     public String hello() {
         return "Hello";
     }
 
-    @GetMapping("user/welcome2")
+    @GetMapping("welcome2")
     public String hello2() {
         return "Hello 2";
     }
