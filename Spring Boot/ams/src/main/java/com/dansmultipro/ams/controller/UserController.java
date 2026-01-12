d36c98be8f1feb4bfb68a1ffe9af50e9262a00d5
imammunf <imammunif17@gmail.com>
2026-01-12 10:10:52 +0700

    day 13: get controller (add specific path)

--- Spring Boot/ams/src/main/java/com/dansmultipro/ams/controller/UserController.java
+++ Spring Boot/ams/src/main/java/com/dansmultipro/ams/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping
+    @GetMapping("welcome")
     public String hello() {
         return "Hello";
     }
