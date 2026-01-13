1a7c086ae6bf1f078b165b9fd041a3b093b161bd
imammunf <imammunif17@gmail.com>
2026-01-13 11:58:55 +0700

    day 14: task (complete the base model)

--- Spring Boot/ams/src/main/java/com/dansmultipro/ams/model/BaseModel.java
+++ Spring Boot/ams/src/main/java/com/dansmultipro/ams/model/BaseModel.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -19,8 +20,17 @@ public abstract class BaseModel {
     @Column(nullable = false)
     private Integer version;
 
+    @Column(nullable = false, length = 36)
+    private String createdBy;
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(length = 36)
+    private String updatedBy;
+
+    @Column
+    private LocalDateTime updatedAt;
 
     public UUID getId() {
         return id;
@@ -38,12 +48,36 @@ public abstract class BaseModel {
         this.version = version;
     }
 
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
 }
