fdbb4c69122155084fbfe12d28d57f8012c42ab0
imammunf <imammunif17@gmail.com>
2026-01-14 11:49:28 +0700

    day 14: company controller (add getter)

--- Spring Boot/ams/src/main/java/com/dansmultipro/ams/dto/company/CompanyResponseDto.java
+++ Spring Boot/ams/src/main/java/com/dansmultipro/ams/dto/company/CompanyResponseDto.java
@@ -12,4 +12,12 @@ public class CompanyResponseDto {
         this.name = name;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
