ace81fc2f68e5f295ece392be0f62e4e1341e30e
imammunf <imammunif17@gmail.com>
2026-01-20 18:45:50 +0700

    day 17: push whatever

--- Spring Boot/ams/src/test/java/com/dansmultipro/ams/service/unit/AssignmentService.java
+++ Spring Boot/ams/src/test/java/com/dansmultipro/ams/service/unit/AssignmentService.java
@@ -4,19 +4,25 @@ import com.dansmultipro.ams.dto.assignment.AssignmentRequestDto;
 import com.dansmultipro.ams.model.Asset;
 import com.dansmultipro.ams.model.Assignment;
 import com.dansmultipro.ams.model.AssignmentDetail;
-import com.dansmultipro.ams.repository.*;
+import com.dansmultipro.ams.model.Location;
+import com.dansmultipro.ams.repository.AssetRepo;
+import com.dansmultipro.ams.repository.AssignmentDetailRepo;
+import com.dansmultipro.ams.repository.AssignmentRepo;
+import com.dansmultipro.ams.repository.LocationRepo;
 import com.dansmultipro.ams.service.impl.AssignmentServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@ExtendWith(MockitoExtension.class)
 public class AssignmentService {
 
     @Mock
@@ -24,9 +30,6 @@ public class AssignmentService {
 
     @Mock
     private AssetRepo assetRepo;
-
-    @Mock
-    private EmployeeRepo employeeRepo;
 
     @Mock
     private AssignmentRepo assignmentRepo;
@@ -39,32 +42,36 @@ public class AssignmentService {
 
     @Test
     public void shouldCreated_whenDataValid() {
-        var dto = new AssignmentRequestDto();
-        List<String> detailIdList = new ArrayList<>();
-        String detailId1 = "29133436-2f2d-486b-8553-8ececd2d17e8";
-        detailIdList.add(detailId1);
+        var location = new Location();
+        location.setId(UUID.randomUUID());
+        location.setName("Room 3007 Soho Building");
 
-        dto.setTargetAssetId(UUID.randomUUID().toString());
-        dto.setAssignmentDetialIdList(detailIdList);
+        var asset = new Asset();
+        asset.setId(UUID.randomUUID());
+        asset.setName("TV LG 32 INCH");
+
+        var dto = new AssignmentRequestDto();
+        dto.setTargetLocationId(location.getId().toString());
+        dto.setAssignmentDetialIdList(List.of(asset.getId().toString()));
 
         var assignmentSaved = new Assignment();
         var id = UUID.randomUUID();
         assignmentSaved.setId(id);
 
-        var asset1 = new Asset();
-        asset1.setName("TV LG 32 INCH");
         var assignmentDetail = new AssignmentDetail();
-        assignmentDetail.setAsset(asset1);
+        assignmentDetail.setAsset(asset);
         assignmentDetail.setAssignment(assignmentSaved);
 
-        Mockito.when(assetRepo.findById(Mockito.any())).thenReturn(Optional.of(new Asset()));
+        Mockito.when(locationRepo.findById(Mockito.any())).thenReturn(Optional.of(location));
+        Mockito.when(assetRepo.findById(Mockito.any())).thenReturn(Optional.of(asset));
         Mockito.when(assignmentRepo.save(Mockito.any())).thenReturn(assignmentSaved);
         Mockito.when(assignmentDetailRepo.save(Mockito.any())).thenReturn(assignmentDetail);
 
         var result = assignmentService.insert(dto);
 
         Assertions.assertEquals(id, result.getId());
-        Mockito.verify(assetRepo, Mockito.atLeast(1)).save(Mockito.any());
+        Mockito.verify(locationRepo, Mockito.atLeast(1)).findById(Mockito.any());
+        Mockito.verify(assetRepo, Mockito.atLeast(1)).findById(Mockito.any());
         Mockito.verify(assignmentRepo, Mockito.atLeast(1)).save(Mockito.any());
         Mockito.verify(assignmentDetailRepo, Mockito.atLeast(1)).save(Mockito.any());
 
