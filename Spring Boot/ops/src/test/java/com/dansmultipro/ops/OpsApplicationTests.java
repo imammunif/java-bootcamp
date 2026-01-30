4ba1796d1ee7c85c4c306af4f300d81f089b61b1
imammunif <imammunif17@gmail.com>
2026-01-30 09:46:47 +0700

    day 26: new online payment system assignment

---
+++ Spring Boot/ops/src/test/java/com/dansmultipro/ops/OpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dansmultipro.ops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
