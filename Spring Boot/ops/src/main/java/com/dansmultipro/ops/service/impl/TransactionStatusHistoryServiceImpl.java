8f4cfdd74686e2d644edf37bd8478dd0e7a48c27
imammunf <imammunif17@gmail.com>
2026-02-03 14:33:47 +0700

    day 28: bug fix history service (get all)

--- Spring Boot/ops/src/main/java/com/dansmultipro/ops/service/impl/TransactionStatusHistoryServiceImpl.java
+++ Spring Boot/ops/src/main/java/com/dansmultipro/ops/service/impl/TransactionStatusHistoryServiceImpl.java
@@ -2,8 +2,9 @@ package com.dansmultipro.ops.service.impl;
 
 import com.dansmultipro.ops.dto.transactionstatushistory.TransactionStatusHistoryResponseDto;
 import com.dansmultipro.ops.exception.NotFoundException;
+import com.dansmultipro.ops.model.GatewayUser;
 import com.dansmultipro.ops.model.TransactionStatusHistory;
-import com.dansmultipro.ops.repository.GatewayRepo;
+import com.dansmultipro.ops.repository.GatewayUserRepo;
 import com.dansmultipro.ops.repository.TransactionStatusHistoryRepo;
 import com.dansmultipro.ops.service.TransactionStatusHistoryService;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,11 @@ import java.util.UUID;
 public class TransactionStatusHistoryServiceImpl extends BaseService implements TransactionStatusHistoryService {
 
     private final TransactionStatusHistoryRepo transactionStatusHistoryRepo;
-    private final GatewayRepo gatewayRepo;
+    private final GatewayUserRepo gatewayUserRepo;
 
-    public TransactionStatusHistoryServiceImpl(TransactionStatusHistoryRepo transactionStatusHistoryRepo, GatewayRepo gatewayRepo) {
+    public TransactionStatusHistoryServiceImpl(TransactionStatusHistoryRepo transactionStatusHistoryRepo, GatewayUserRepo gatewayUserRepo) {
         this.transactionStatusHistoryRepo = transactionStatusHistoryRepo;
-        this.gatewayRepo = gatewayRepo;
+        this.gatewayUserRepo = gatewayUserRepo;
     }
 
     @Override
@@ -37,12 +38,12 @@ public class TransactionStatusHistoryServiceImpl extends BaseService implements 
 
     @Override
     public List<TransactionStatusHistoryResponseDto> getAllByGatewayId() {
-        UUID gatewayId = principalService.getPrincipal().getId();
-        gatewayRepo.findById(gatewayId).orElseThrow(
-                () -> new NotFoundException("Gateway not found")
+        UUID userId = principalService.getPrincipal().getId();
+        GatewayUser gatewayUser = gatewayUserRepo.findByUserId(userId).orElseThrow(
+                () -> new NotFoundException("Gateway user not found")
         );
 
-        List<TransactionStatusHistory> historyList = transactionStatusHistoryRepo.findAllByTransaction_GatewayId(gatewayId);
+        List<TransactionStatusHistory> historyList = transactionStatusHistoryRepo.findAllByTransaction_GatewayId(gatewayUser.getGateway().getId());
         List<TransactionStatusHistoryResponseDto> historyResponseDtoList = new ArrayList<>();
         for (TransactionStatusHistory v : historyList) {
             TransactionStatusHistoryResponseDto responseDto = new TransactionStatusHistoryResponseDto(
